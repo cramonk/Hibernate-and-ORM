3b400010aae09618575ee9a379ecc3c8eeb24afb
cramonk <cramonk59@gmail.com>
2024-07-18 15:19:10 +0300

    Практическая задача 2.2.1 Hibernate и ORM.

---
+++ src/main/java/hiber/MainApp.java
@@ -0,0 +1,45 @@
+package hiber;
+
+import hiber.config.AppConfig;
+import hiber.model.Car;
+import hiber.model.User;
+import hiber.service.UserService;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import java.sql.SQLException;
+import java.util.List;
+
+public class MainApp {
+    public static void main(String[] args) throws SQLException {
+        AnnotationConfigApplicationContext context =
+                new AnnotationConfigApplicationContext(AppConfig.class);
+
+        UserService userService = context.getBean(UserService.class);
+
+        User user1 = new User("User1", "Lastname1", "user1@mail.ru");
+        User user2 = new User("User2", "Lastname2", "user2@mail.ru");
+        User user3 = new User("User3", "Lastname3", "user3@mail.ru");
+        User user4 = new User("User4", "Lastname4", "user4@mail.ru");
+
+        user1.setCar(new Car("FIRST", 101));
+        user2.setCar(new Car("SECOND", 202));
+        user3.setCar(new Car("THIRD", 303));
+        user4.setCar(new Car("FORTH", 404));
+
+        userService.add(user1);
+        userService.add(user2);
+        userService.add(user3);
+        userService.add(user4);
+
+        List<User> users = userService.listUsers();
+
+        for (User user : users) {
+            System.out.println(user);
+            System.out.println();
+        }
+
+        User userByCar = userService.getByCar("THIRD", 303);
+        System.out.println("Найден пользователь - " + userByCar);
+        context.close();
+    }
+}
